a0799df518a8d059b6acf81c4108db5b6229c3d6
John Fedoruk <johnny@johnfedoruk.ca>
2018-10-16 20:01:22 -0500

    Implemented video player

--- projects/ng-video-player/src/lib/ng-video-player.component.ts
+++ projects/ng-video-player/src/lib/ng-video-player.component.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'ng-video-player',
-  template: `
-    <p>
-      video-player works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './ng-video-player.component.html',
+  styleUrls: ['./ng-video-player.component.css']
 })
-export class NgVideoPlayerComponent implements OnInit {
+export class NgVideoPlayerComponent {
+
+  public src: string | undefined;
+  public show: boolean;
+
+  @Input('video')
+  public set video(src: string | undefined) {
+    if (!!this.src) { // already showing a video
+      if (!!src) { // change video
+        this.src = src;
+        this.videoChange.emit(src);
+      } else { // remove video
+        this.src = undefined;
+        this.videoChange.emit(undefined);
+        this.show = false;
+      }
+    } else { // not already showing a video
+      if (!!src) { // start video
+        this.src = src;
+        this.videoChange.emit(src);
+        this.show = true;
+      }
+    }
+  }
+
+  @Output()
+  public videoChange = new EventEmitter<string|undefined>();
+
+  public set hide(hide: boolean) {
+    if (hide === true) {
+      this.video = undefined;
+    }
+  }
+
+  public stopPropagation($event: Event): void {
+    $event.stopPropagation();
+  }
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
