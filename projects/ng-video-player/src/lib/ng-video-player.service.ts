b771ce7bf03ab5f58856ff091105ac577fa8c8b1
John Fedoruk <johnny@johnfedoruk.ca>
2018-10-16 20:43:23 -0500

    Added state handling to NgVideoPlayerService

--- projects/ng-video-player/src/lib/ng-video-player.service.ts
+++ projects/ng-video-player/src/lib/ng-video-player.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgVideoPlayerService {
 
+  public set play(url: string | undefined | null) {
+    this.playing.next(url);
+  }
+  public playing: Subject<string> = new BehaviorSubject<string>(undefined);
   constructor() { }
 }
