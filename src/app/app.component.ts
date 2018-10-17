6ad611bdfc2621b1706b48d079413d1db745652f
John Fedoruk <johnny@johnfedoruk.ca>
2018-10-16 20:44:31 -0500

    Updated demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgVideoPlayerService } from 'ng-video-player';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'NG Video Player';
   public video: string;
-  public playVideo(): void {
+  constructor(private player: NgVideoPlayerService) { }
+  public playVideo1(): void {
     this.video = 'https://www.w3schools.com/html/mov_bbb.mp4';
+  }
+  public playVideo2(): void {
+    this.player.play = 'https://www.w3schools.com/tags/movie.mp4';
   }
 }
