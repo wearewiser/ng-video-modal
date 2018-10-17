4951725fff191b2bd40ee5894d29cef8c718fca4
John Fedoruk <johnny@johnfedoruk.ca>
2018-10-16 20:00:57 -0500

    Importing CommonModule into NgVideoPlayerModule

--- projects/ng-video-player/src/lib/ng-video-player.module.ts
+++ projects/ng-video-player/src/lib/ng-video-player.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NgVideoPlayerComponent } from './ng-video-player.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule
   ],
   declarations: [NgVideoPlayerComponent],
   exports: [NgVideoPlayerComponent]
